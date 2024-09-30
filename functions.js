ed3e142232f6ff4690ef99104202927dacff0b3a
428770 <428770@edu.cwa.ac.uk>
2024-09-30 16:19:59 +0100

    End Lesson 30/09/24

---
+++ functions.js
@@ -0,0 +1,5 @@
+function splitBill(inTotal, inTip, inNo) {
+    var bill = Number(document.getElementById(inTotal).value);
+    var tip = Number(document.getElementById(inTip).value);
+    var people = Number(document.getElementById(inNo).value);
+}

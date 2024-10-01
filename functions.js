fa0e82188c259f1894e41d23d39e29b88d624a55
428770 <428770@edu.cwa.ac.uk>
2024-10-01 12:17:23 +0100

    Lunch - Not Yet Working

--- functions.js
+++ functions.js
@@ -2,4 +2,16 @@ function splitBill(inTotal, inTip, inNo) {
     var bill = Number(document.getElementById(inTotal).value);
     var tip = Number(document.getElementById(inTip).value);
     var people = Number(document.getElementById(inNo).value);
+    tip = tip / 100;
+    tip = bill * tip;
+    var total = bill + tip;
+    document.getElementById(output).innerHTML = (total / people);
+}
+
+document.getElementById("splitbill").onclick = function() {
+    splitBill(inputTotal, tipSelected, inputPeople);
+}
+
+function getTip(clicked) {
+    var tipSelected = Number(clicked);
 }
